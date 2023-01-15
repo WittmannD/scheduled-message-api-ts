109c189a1aa13260c1a82018221ed397074dbbf9
Danil Wittmann <danjavitman@gmail.com>
2023-01-15 07:58:36 +0200

    specify node version, fix dto

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,15 +1,17 @@
 import { ScheduledMessage } from '@prisma/client';
-import { IsDateString, IsEmpty, Length } from 'class-validator';
+import { IsDateString, IsOptional, IsString, Length } from 'class-validator';
 import { MinDate } from '../../decorators/validate-min-date.decorator';
 
 export class CreateMessageDto
   implements Pick<ScheduledMessage, 'content' | 'payload' | 'timeForDispatch'>
 {
+  @IsString()
   @Length(1, 3000)
   content: string;
 
-  @IsEmpty()
-  payload: string;
+  @IsString()
+  @IsOptional()
+  payload: string | null;
 
   @IsDateString()
   @MinDate()
